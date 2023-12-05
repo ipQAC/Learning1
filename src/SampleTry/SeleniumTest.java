7acfc6be325b725c311d1220ece87f9962bb97e4
Ilesh Patel <mail@example.com>
2023-12-05 11:09:24 -0600

    Optimize import statements

--- src/SampleTry/SeleniumTest.java
+++ src/SampleTry/SeleniumTest.java
@@ -1,13 +1,10 @@
 package SampleTry;
 
-import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
 
